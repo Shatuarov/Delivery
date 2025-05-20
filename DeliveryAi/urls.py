7b4f2f88d482f8114243f97bb68a45e462f04116
Нұрқанат Шатуаров <alimbetovnurik3596@gmail.com>
2025-05-21 05:20:57 +0600

    f

--- DeliveryAi/urls.py
+++ DeliveryAi/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', include('api.urls')),
     path('', include('main.urls')),
     path('orders/', include('orders.urls')),
+    path('', include("chatbot.urls")),
     path('restaurants/', include('restaurants.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
